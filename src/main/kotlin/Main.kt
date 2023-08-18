efab05ee9076e309aebf3e2ef0d4ce4f35592c91
mohamedali <soft.mohamedali@gmail.com>
2023-08-18 15:55:16 +0300

    stack based on array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 import stack.StackBasedArray
 
 fun main(args: Array<String>) {
-
+    val m=StackBasedArray<String>(3)
+    m.push("dsf")
+    m.print()
+    m.push("r")
+    m.print()
+    m.push("v")
+    println("pop = ${m.getTop()}")
+    m.pop()
+    m.print()
+    m.push("vv")
+    m.print()
 }
